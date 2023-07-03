919dd03344852d2ac5579178017a95657b17f766
Jerome A <je.aprile@gmail.com>
2023-07-03 15:22:10 +0200

    Ajout de la EditPage (HTML+JS)

---
+++ EditPage/EditPage.js
@@ -0,0 +1,159 @@
+
+const FormName = document.getElementById("FormName");
+const FormShortDescription = document.getElementById("FormShortDescription");
+const FormDescription = document.getElementById("FormDescription");
+const FormImage = document.getElementById("FormImage");
+let ExistingImage;
+
+/*
+const id = (new URLSearchParams(window.location.search)).get("id")
+    // Window = fenêtre actuelle/page Location = tout ce qui se rapporte à l'url
+
+fetch(`https://character-database.becode.xyz/characters/${id}`)
+*/
+
+const URL = `https://character-database.becode.xyz/characters/086a6d93-9861-418d-aed2-8b1a9e31460b`;
+
+//Récupère les informations initiales du perso
+fetch(URL)
+.then(response => response.json())
+.then(data => {
+    
+    FormName.defaultValue = data.name;
+
+    FormShortDescription.defaultValue = data.shortDescription;
+    FormDescription.defaultValue = data.description;
+    FormImage.defaultValue = data.image;
+    const image = data.image;
+    FormImage.src = `data:image/png;base64, ${image}`;
+    ExistingImage = image;
+
+})
+.catch(error => {
+  console.log("Une erreur s'est produite lors de la récupération des informations...", error);
+});
+
+
+//Récupère les nouvelles infos dans le formulaire en cliquant sur le btn submit
+const EditForm = document.getElementById("CharacterForm");
+
+
+
+EditForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Empêche le rechargement de la page après la soumission du formulaire
+
+const ImageInput = document.getElementById("FormNewImage");
+  if (ImageInput.files.length === 0) { //Si aucun fichier n'a été chargé
+
+  const NameInput = document.getElementById("FormName");
+  const ShortDescriptionInput = document.getElementById("FormShortDescription");
+  const DescriptionInput = document.getElementById("FormDescription");
+
+    const NameValue = NameInput.value.trim();
+    const ShortDescriptionValue = ShortDescriptionInput.value.trim();
+    const DescriptionValue = DescriptionInput.value.trim();
+
+    //crée un tableau Json avec les données récoltées
+    const updatedCharacter = {
+      name: NameValue,
+      shortDescription: ShortDescriptionValue,
+      description: DescriptionValue,
+      image : ExistingImage,
+    };
+
+    //PUT le nouveau tableau Json
+  fetch(URL, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedCharacter),
+  })
+
+  .then(response => {
+    if (response.ok) {
+      console.log("Le personnage a été mis à jour avec succès sans la photo !");
+    } else {
+      console.log("Une erreur s'est produite lors de la mise à jour du personnage sans la photo.");
+    }
+  })
+
+  .catch(error => {
+    console.log("Une erreur s'est produite lors de la requête PUT :", error);
+  });
+
+
+
+
+} else { //Si un fichier a été chargé
+
+  const NameInput = document.getElementById("FormName");
+  const ShortDescriptionInput = document.getElementById("FormShortDescription");
+  const DescriptionInput = document.getElementById("FormDescription");
+  const ImageInput = document.getElementById("FormNewImage");
+
+  const file = ImageInput.files[0];
+  const ImageValue = ImageInput.value;
+
+  const reader = new FileReader();
+
+
+  reader.onload = function(event) {
+    const base64Text = event.target.result;
+    const base64WithoutHeader = base64Text.replace(/^data:image\/[a-z]+;base64,/, '');
+    console.log(base64WithoutHeader); 
+
+    const NameValue = NameInput.value.trim();
+    const ShortDescriptionValue = ShortDescriptionInput.value.trim();
+    const DescriptionValue = DescriptionInput.value.trim();
+
+
+    const updatedCharacter = {
+      name: NameValue,
+      shortDescription: ShortDescriptionValue,
+      description: DescriptionValue,
+      image : base64WithoutHeader,
+    
+    };
+
+
+    fetch(URL, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedCharacter),
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log("Le personnage a été mis à jour avec succès avec la photo !");
+          // Traitez ici la réponse en cas de succès de la modification
+        } else {
+          console.log("Une erreur s'est produite lors de la mise à jour du personnage avec la photo.");
+          // Traitez ici la réponse en cas d'échec de la modification
+        }
+      })
+      .catch(error => {
+        console.log("Une erreur s'est produite lors de la requête PUT :", error);
+      });
+  
+  
+    }
+   
+    reader.readAsDataURL(file);
+
+
+  }
+  
+});
+
+
+
+
+  
+  
+
+
+ 
+
+
--- SinglePage/SinglePage.js
+++ SinglePage/SinglePage.js
@@ -1,118 +1,4 @@
-const characterForm = document.getElementById('CharacterForm');
 
-characterForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Empêche l'envoi du formulaire par défaut
-  
-    const form = document.getElementById('CharacterForm');
-    const name = form.elements.name.value;
-    const shortDescription = form.elements.shortDescription.value;
-    const description = form.elements.description.value;
-  
-    const fileInput = form.elements.image; // Récupère l'élément d'entrée de fichier
-    const file = fileInput.files[0]; // Récupère le fichier sélectionné par l'utilisateur
-  
-    const reader = new FileReader(); // Crée une instance de FileReader
-  
-    reader.onload = function(event) {
-      const imageData = event.target.result.split(',')[1]; // Extrait les données Base64 de l'URL de données
-      
-      // Appelle la fonction createCharacter avec les données du personnage et l'image encodée en Base64
-      createCharacter(name, shortDescription, description, imageData);
-    };
-  
-    reader.readAsDataURL(file); // Lit le contenu du fichier en tant qu'URL de données
-});
-
-function createCharacter(name, shortDescription, description, imageData) {
-  const characterData = {
-    name: name,
-    shortDescription: shortDescription,
-    description: description,
-    image: imageData
-  };
-
-  // Envoi de la requête POST à l'API pour créer le personnage
-  fetch('https://character-database.becode.xyz/characters', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(characterData)
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log('Nouveau personnage créé avec l\'ID :', data.id);
-    console.log("Le personnage a été créé !");
-  })
-  .catch(error => {
-    console.error('Erreur lors de la création du personnage :', error);
-  });
-}
-
-
-
-  
-/*
-Isn't he lovely ?
-This is my favorite version of Shrek ! 
-*/  
-  
-    
-  
-  
-
-
-/* Le code pour la page avec les informations précises d'un personnage :
-
-const id = (new URLSearchParams(window.location.search)).get("id")
-    // Window = fenêtre actuelle/page Location = tout ce qui se rapporte à l'url
-
-fetch(`https://character-database.becode.xyz/characters/${id}`)
-.then(response => response.json())
-.then(data => {
-    const CharactersContainer = document.querySelector(".CharactersContainer");
-
-    const CharacterColumn = document.createElement("div");
-    CharacterColumn.classList.add("col-4")
-    CharactersContainer.appendChild(CharacterColumn);
-
-    const CharacterCard = document.createElement("div");
-    CharacterCard.classList.add("card")
-    CharacterCard.classList.add("mb-4")
-    CharacterColumn.appendChild(CharacterCard);
-    
-            //Récupère l'image du personnage
-            const image = data.image;
-            const Characterimg = document.createElement("img");
-            Characterimg.classList.add("CharacterImage");
-            Characterimg.setAttribute("src", `data:image/png;base64, ${image}`);
-            CharacterCard.appendChild(Characterimg);
-
-            //Récupère le nom du personnage
-            const CharacterName = document.createElement("h5");
-            CharacterName.classList.add("CharacterName");
-            CharacterName.classList.add("card-title");
-            CharacterName.textContent = data.name;
-            CharacterCard.appendChild(CharacterName);
-
-            //Récupère la courte description du personnage
-            const CharacterShortDescription = document.createElement("p");
-            CharacterShortDescription.classList.add("CharacterShortDescription");
-            CharacterShortDescription.textContent = data.shortDescription;
-            CharacterShortDescription.classList.add("card-text");
-            CharacterCard.appendChild(CharacterShortDescription);
-
-            //Récupère la courte description du personnage
-            const CharacterDescription = document.createElement("p");
-            CharacterDescription.classList.add("CharacterDescription");
-            CharacterDescription.textContent = data.description;
-            CharacterDescription.classList.add("card-text");
-            CharacterCard.appendChild(CharacterDescription);
-})
-*/
-
-
-/* Code pour la SinglePage :
 
 fetch("https://character-database.becode.xyz/characters")
 .then(response => response.json())
@@ -163,7 +49,7 @@ fetch("https://character-database.becode.xyz/characters")
         CharacterID.classList.add("CharacterID");
         CharacterID.textContent = data[i].id;
         CharacterCard.appendChild(CharacterID);
-        CharacterID.style.display = "none";
+        //CharacterID.style.display = "none";
 
         //Crée un boutton "View more"
         const VMButton = document.createElement("a");
@@ -200,4 +86,3 @@ searchForm.addEventListener("submit", function (event) {
     }
   });
 });
-*/
