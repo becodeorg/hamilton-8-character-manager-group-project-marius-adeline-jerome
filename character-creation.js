4926d42f2c46c789df7d2966f747d81cb0f2a0f7
Adeline Merle <adeline.merle1@gmail.com>
2023-07-06 21:40:20 +0200

    add Vite

--- character-creation.js
+++ character-creation.js
@@ -1,3 +1,4 @@
+import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css";
 import { setupFileUpload } from './modules-js/image-upload.js';
 import { setupCreationButton } from './modules-js/creation-button.js';
 
@@ -9,6 +10,3 @@ function initializeApp() {
   setupFileUpload();
   setupCreationButton();
 }
-
-
-
