45da4261f921ec76c177ac57fb5ed7e073901941
Adeline Merle <adeline.merle1@gmail.com>
2023-07-08 21:09:21 +0200

    readd bootstrap

--- character-creation.js
+++ character-creation.js
@@ -1,6 +1,6 @@
 
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import './main.scss';
 
 import { setupFileUpload } from './modules-js/image-upload.js';
